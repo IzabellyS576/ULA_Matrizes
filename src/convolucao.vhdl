9197d6c548fe0436789b0c854c9b4bdd34b6da66
Belly <izabellysouza576@gmail.com>
2026-06-21 15:02:32 -0300

    code convolucao module and adjust tb

---
+++ src/convolucao.vhdl
@@ -0,0 +1,20 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.numeric_std.all;
+USE work.ula_pack.all;
+
+ENTITY convolucao IS
+    GENERIC(
+		W : positive := 8
+	);
+	PORT (
+		in_A     : IN  signed(W-1 DOWNTO 0);
+		in_B     : IN  signed(W-1 DOWNTO 0);
+		product : OUT signed(2*W-1 DOWNTO 0)
+	);
+END convolucao;
+
+ARCHITECTURE arch OF convolucao IS
+    product <= in_A * in_B;
+
+END ARCHITECTURE; -- arch
